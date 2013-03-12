e482f20347ed5601f229f686a111485b3a2fc3ad
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-03-12 14:42:12 +0100

    adds left-right-selection to changescreen

--- changescreen.sh
+++ changescreen.sh
@@ -4,9 +4,21 @@ intdisplay="LVDS1"
 extdisplay="VGA1"
 
 if [ "$1" = "int" ]; then
-	xrandr --output $intdisplay --auto --output $extdisplay --off
+    xrandr --output $intdisplay --auto --output $extdisplay --off
 elif [ "$1" = "ext" ]; then
-	xrandr --output $extdisplay --auto --output $intdisplay --off
+    xrandr --output $extdisplay --auto --output $intdisplay --off
+elif [ "$1" = "both" ]; then
+    if [ "$2" = "ext" ]; then
+        if [ "$3" = "right" ]; then
+            xrandr --output $intdisplay --auto --output $extdisplay --auto --right-of $intdisplay
+        elif [ "$3" = "left" ]; then
+            xrandr --output $intdisplay --auto --output $extdisplay --auto --left-of $intdisplay
+        else
+            echo Command Line options not recoginzed. kthxbai
+        fi
+    else
+        echo Command line options not recognized. kthxbai
+    fi
 else
-	echo Comman line options not recognized. kthxbai. 
+    echo Command line options not recognized. kthxbai. 
 fi
